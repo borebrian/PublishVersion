7334328f782c703ffa9f8d1880966eaefb20fc49
borebrian <borebrian77@gmail.com>
2019-10-20 23:53:22 -0700

    Fixed bugs in video natives

--- app/src/main/java/com/borebrian/statussaver/video/VideoActivity.kt
+++ app/src/main/java/com/borebrian/statussaver/video/VideoActivity.kt
@@ -212,6 +212,7 @@ class VideoActivity : AppCompatActivity(), Player.EventListener {
         // You must call destroy on old ads when you are done with them,
         // otherwise you will have a memory leak.
         currentNativeAd?.destroy()
+        
         currentNativeAd = nativeAd
         // Set the media view. Media content will be automatically populated in the media view once
         // adView.setNativeAd() is called.
